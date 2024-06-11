65e5066a1d14cd0c07fb2c38dd9ba80a9f4b4517
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-11 17:22:55 +0200

    Dome: add logical diagram

---
+++ main.js
@@ -0,0 +1,10 @@
+//Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while
+//seleziono il mio contenitore lista dal DOM
+
+//creao la mia lista della spesa
+
+//creo il mio ciclo while
+
+//creo gli elementi li
+
+//inserisco la mia lista dell'html
