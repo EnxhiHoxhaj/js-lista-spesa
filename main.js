4d5f73d3515867bfc3dd2ac3a1e2542f4ffe938a
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-11 17:28:48 +0200

    Feat: select tag ul in Html file

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while
 //seleziono il mio contenitore lista dal DOM
 
+let foglioSpesa = document.querySelector("ul");
+console.log(foglioSpesa);
+
 //creao la mia lista della spesa
 
 //creo il mio ciclo while
